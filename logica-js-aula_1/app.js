b4364a6bd1c483e9c180aa2083b85b3ec9d64a9f
Niedja92 <niedleticia@hotmail.com>
2024-06-21 22:12:16 -0300

    Initial commit

---
+++ logica-js-aula_1/app.js
@@ -0,0 +1,50 @@
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um número entre 1 e 10');
+
+// if (chute == numeroSecreto) {
+//     console.log('Isso ai! Você descobriu o número secreto (5)');
+// }
+
+// DESAFIOS
+
+// 1 - Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+
+alert('Qual é o dia da semana?')
+
+diaDaSemana = prompt('Qual é o dia da semana?')
+if(diaSemana == 'Sábado') {
+    alert('Bom fim de semana!')
+} else if (diaSemana == 'Domingo') {
+    alert('Bom fim de semana!')
+} else {
+    alert('Boa semana!')
+}
+
+// 2 - Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
+}
+
+// 3 - Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!" no console do navegador. Caso contrário, mostre "Tente novamente para ganhar.".
+
+pontuacao = 105;
+if (pontuacao >= 100) {
+    console.log('Parabéns, você venceu!');
+} else {
+    console.log('Tente novamente para ganhar.');
+}
+
+// 4 - Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);
+
+// 5 - Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+
+let nome = prompt('Qual o seu nome?');
+alert(`Boas vindas ${nome}`);
---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,23 @@
+// alert('Boas vindas ao jogo do número secreto!')
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um número entre 1 e 10');
+
+// if(chute == numeroSecreto) {
+//     console.log('Isso aí! Você descobriu o número secreto ((5))');
+// }
+
+
+// Desafios Lógica de programação - Iniciando com JavaScript
+
+// alert("Boas vindas ao nosso site!");
+
+// let idade = 18;
+// let maiorDeIdade = prompt('Digite a sua idade')
+
+// if(maiorDeIdade >= idade) {
+//     alert('Pode tirar a habilitação')
+// } else (maiorDeIdade <= idade) ;{
+//     alert('Infelizmente a sua idade não permitir tirar a habilitação.');
+// };
+
+
